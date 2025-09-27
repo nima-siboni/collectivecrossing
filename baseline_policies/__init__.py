1cce66a139add9b68fbcfe28065714e39a8a0b4e
semi-ergodic <nima.siboni@gmail.com>
2025-09-27 23:11:15 +0200

    moving codes around

--- baseline_policies/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Baseline policies for the collective crossing environment."""
-
-from .greedy_policy import GreedyPolicy, create_greedy_policy
-
-__all__ = ["GreedyPolicy", "create_greedy_policy"]
